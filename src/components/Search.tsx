8979d7415c27a35caa183f6c1ac1f4902fc6ab5e
Tania Ballester <tballester.email@gmail.com>
2023-04-04 11:52:34 +0200

    style: apply styles to search input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,10 @@
 const Search = (props: SearchProps) => (
-  <>
-    <span>{props.results.length}</span>
-    <input type="search" value={props.searchText} placeholder="Filter podcasts..." onChange={(e) => props.handleChange(e)} />
-  </>
+  <div className="search">
+    <div className="search__filter">
+      <span className="search__badge">{props.results.length}</span>
+      <input className="search__input" type="search" value={props.searchText} placeholder="Filter podcasts..." onChange={(e) => props.handleChange(e)} />
+    </div>
+  </div>
 )
 
 export default Search;
