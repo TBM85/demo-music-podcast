8eecba3615a451223f0c39172930e7c81700c9bd
Tania Ballester <tballester.email@gmail.com>
2023-04-05 18:42:12 +0200

    style: apply to not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h2 className="not-found__title">Error 404</h2>
-      <p className="not-found__message">Sorry, we couldn't find this page.</p>
-      <Link to="/" className="not-found__link">
-        Back to homepage
-      </Link>
+      <div className="not-found__container">
+        <h2 className="not-found__title">Error 404</h2>
+        <p className="not-found__message">Sorry, we couldn't find this page.</p>
+        <Link to="/" className="not-found__link card">
+          Back to homepage
+        </Link>
+      </div>
     </div>
   );
 };
