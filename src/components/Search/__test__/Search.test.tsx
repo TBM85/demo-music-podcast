f0cc5c8e1a5aca43fb5adc6c2c22c787b083ab4b
Tania Ballester <tballester.email@gmail.com>
2023-04-12 15:05:08 +0200

    test: updates the input value

--- src/components/Search/__test__/Search.test.tsx
+++ src/components/Search/__test__/Search.test.tsx
@@ -15,12 +15,21 @@ describe("Search", () => {
   it("should calls the handleChange function when the input value changes", async () => {
     render(<Search {...mockProps} />);
     const inputElement = screen.getByPlaceholderText(
-      "Filter podcasts..."
+      /filter podcasts.../i
     ) as HTMLInputElement;
     const newValue = "new value";
     fireEvent.change(inputElement, { target: { value: newValue } });
     inputElement.value = newValue;
     expect(handleChangeMock).toHaveBeenCalledWith(expect.any(Object));
     expect(handleChangeMock.mock.calls[0][0].target.value).toBe(newValue);
+  });
+
+  it("should updates the input value when the searchText prop changes", () => {
+    const newProps: SearchProps = { ...mockProps, searchText: "new value" };
+    render(<Search {...newProps} />);
+    const inputElement = screen.getByPlaceholderText(
+      /filter podcasts.../i
+    ) as HTMLInputElement;
+    expect(inputElement.value).toBe("new value");
   });
 });
